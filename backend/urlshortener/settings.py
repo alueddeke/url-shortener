910fba5804c5ed5d82c0d189460e183fa78eab0d
alueddeke <a.lueddeke@hotmail.com>
2025-07-14 09:35:46 -0400

    created backend api

--- backend/urlshortener/settings.py
+++ backend/urlshortener/settings.py
@@ -1,5 +1,8 @@
 
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--hi0!$8aiouqkgnikf-&^-w#fl*7rbjo^b0n(=&ll!d0#x-&v#"
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
